2ddda42ddd0a5a1890f7f7e095a724a21d428ab5
caomingdong <18270749739@163.com>
2019-08-18 16:58:31 +0800

    基础样式

--- src/components/Tabbar/index.vue
+++ src/components/Tabbar/index.vue
@@ -23,12 +23,16 @@ export default {
   height: 50px;
   width: 100%;
   display: flex;
+  -moz-box-shadow: -1px -10px 10px #d9d4d4;
+  -webkit-box-shadow: -1px -10px 10px #d9d4d4;
+  box-shadow: -1px -2px 10px #d9d4d4;
   .cz-tabbar_bot {
     width: 100%;
     height: 100%;
     display: flex;
     box-sizing: border-box;
   }
+
   li {
     flex: 1;
     user-select: none;
@@ -46,11 +50,15 @@ export default {
         color: #8f918f;
       }
     }
-
     .iconfont {
       font-size: 24px;
       max-height: 24px;
     }
   }
+}
+.cz-tabbar_bot li a:focus {
+  background: #f3f1ef;
+  background: -webkit-linear-gradient(to bottom, #f3f1ef, #ffffff);
+  background: linear-gradient(to bottom, #f3f1ef, #ffffff);
 }
 </style>
