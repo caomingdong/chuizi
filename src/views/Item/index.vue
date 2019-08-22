ea8dda6dc608298ae2c9176228df57f96eae1c97
Zhajun882 <2783739306@qq.com>
2019-08-22 19:10:48 +0800

    详情页跳转问题更新

--- src/views/Item/index.vue
+++ src/views/Item/index.vue
@@ -12,18 +12,14 @@
 import ItemList from "../../components/ItemList/index";
 import { mapActions, mapState } from "vuex";
 export default {
-<<<<<<< HEAD
-  name: 'Item',
+  name: "Item",
   watch: {
     $route(newVal, oldVal) {
-      let ids = this.$route.params.itemId
-      ids = ids.substring(0, ids.length - 2)
-      this.getItemList(ids)
+      let ids = this.$route.params.itemId;
+      ids = ids.substring(0, ids.length - 2);
+      this.getItemList(ids);
     }
   },
-=======
-  name: "Item",
->>>>>>> ecbe536c18143423553c420f25df5601187fa431
   components: {
     ItemList
   },
@@ -31,7 +27,7 @@ export default {
     ...mapActions("category", ["getItemList"])
   },
   computed: {
-    ...mapState('category', ['ItemList', 'RecommendList'])
+    ...mapState("category", ["ItemList", "RecommendList"])
   },
   created() {
     let ids = this.$route.params.itemId;
